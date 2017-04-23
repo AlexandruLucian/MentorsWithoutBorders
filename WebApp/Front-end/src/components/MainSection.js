e62aa17fd48427a836a76100ca293da7b589a53d
Edmond Pruteanu <edmond@swiftshift.com>
2017-04-23 10:32:20 +0300

    Reversed order of todos

--- WebApp/Front-end/src/components/MainSection.js
+++ WebApp/Front-end/src/components/MainSection.js
@@ -67,7 +67,7 @@ export default class MainSection extends Component {
       <section className="main">
         {this.renderToggleAll(completedCount)}
         <ul className="todo-list">
-          {filteredTodos.map(todo =>
+          {filteredTodos.reverse().map(todo =>
             <TodoItem key={todo.id} todo={todo} {...actions} />
           )}
         </ul>
